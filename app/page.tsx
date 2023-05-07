490942f55a3ba9dd298206f8981b0808bd02c79f
shokouhi <pgulf.611@gmail.com>
2023-05-07 21:59:52 +0430

    add content to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,33 @@
 
 export default function Home() {
   return (
-    <main className="">
-     <div className="">
+    <div className="">
+      <div className="h-[80vh] flex items-center justify-center">
+        <section className="w-2/3 space-y-5">
+          <h1 className="text-5xl">Hi, i'm Saleh Shokouhi</h1>
+          <h2 className="text-3xl">Full Stack Web Developer</h2>
+          <p>With over four years of experience in web development, I am a self-taught professional skilled in crafting dynamic web applications using popular front-end technologies like Vue and React. My proven track record includes effectively leading small teams of mid-level and junior developers. My knowledge of Git, Git flow, and agile methodologies enables me to work efficiently and collaboratively within development teams. Furthermore, I have a proficient understanding of back-end technologies, including NestJS, MySQL, RabbitMQ, and more.</p>
+          <div className="flex">
+            <button className="bg-sky-800 text-white px-5 py-2 rounded-md hover:bg-sky-900">Contact Me</button>
+            <div className="mx-2"></div>
+            <button className="border border-sky-800 text-sky-800 px-5 py-2 rounded-md hover:border-sky-900 hover:text-sky-900">Download My Resume ( pdf )</button>
+          </div>
+        </section>
+        <section className="w-1/3">
 
-     </div>
-    </main>
+        </section>
+      </div>
+      <div className="h-[10vh] flex gap-3">
+        <a href="#" target="_blank" className="rounded-full border-2 border-sky-800 text-center px-3 h-10 flex items-center justify-center hover:bg-sky-800 transition-all duration-150" >
+          <span >linkedin</span>
+        </a>
+        <a href="#" target="_blank" className="rounded-full border-2 border-sky-800 text-center px-3 h-10 flex items-center justify-center hover:bg-sky-800 transition-all duration-150" >
+          <span >github</span>
+        </a>
+        <a href="#" target="_blank" className="rounded-full border-2 border-sky-800 text-center px-3 h-10 flex items-center justify-center hover:bg-sky-800 transition-all duration-150" >
+          <span >stackoverflow</span>
+        </a>
+      </div>
+    </div>
   )
 }
