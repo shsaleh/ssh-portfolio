eeab00cf477b3231f43c8f5b963f103414d64353
shokouhi <pgulf.611@gmail.com>
2023-05-11 00:09:56 +0430

    change style textfiled

--- components/customs/textField/index.tsx
+++ components/customs/textField/index.tsx
@@ -1,12 +1,10 @@
-import React from "react";
+import React, { InputHTMLAttributes } from "react";
 
-type TextFieldPropsType = React.InputHTMLAttributes<HTMLInputElement> & {
-    label?: string;
-};
+type TextFieldPropsType = InputHTMLAttributes<{}>
 export default function TextField(props: TextFieldPropsType) {
     return (
         <>
-            <input {...props} className={"text-black p-1 rounded-sm focus:outline-2 focus:outline-sky-600 outline transition-all ease-in-out duration-150 w-full" + props.className} />
+            <input {...props} className={" p-2 bg-white rounded-sm bg-opacity-0 focus:bg-opacity-10 outline-2 outline-sky-600 outline transition-all ease-in-out duration-150 w-full " + props.className} />
         </>
     );
 }
