44c1b061411b41105314981435137e8238d8b2a9
shokouhi <pgulf.611@gmail.com>
2023-05-11 00:09:15 +0430

    fix image

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import profileImage from "../public/assets/profile.jpg";
 
 export default function Home() {
   return (
@@ -8,13 +9,13 @@ export default function Home() {
         <section className="md:w-1/3 md:flex-row flex-col flex justify-center items-center">
           <div className="block rounded-full overflow-hidden  border-sky-800 w-[80%] h-[80%] ">
             <Image
-              src={require("../public/assets/profile.jpg")}
+              src={profileImage}
               alt="Picture of the author"
             />
           </div>
         </section>
         <section className="md:w-2/3 space-y-5  md:text-left text-justify flex flex-col items-center md:items-start">
-          <h1 className="md:text-5xl text-2xl">Hi, i'm Saleh Shokouhi</h1>
+          <h1 className="md:text-5xl text-2xl">Hi, i&apos;m Saleh Shokouhi</h1>
           <h2 className="md:text-xl text-sky-400">Full Stack Web Developer</h2>
           <p>With over four years of experience in web development, I am a self-taught professional skilled in crafting dynamic web applications using popular front-end technologies like Vue and React. My proven track record includes effectively leading small teams of mid-level and junior developers. My knowledge of Git, Git flow, and agile methodologies enables me to work efficiently and collaboratively within development teams. Furthermore, I have a proficient understanding of back-end technologies, including NestJS, MySQL, RabbitMQ, and more.</p>
           <div className="flex flex-col md:flex-row gap-5">
