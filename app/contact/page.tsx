e77c9985b4f5fd625963277e90f6d8955f7828f2
shokouhi <pgulf.611@gmail.com>
2023-05-13 20:39:11 +0430

    use btn component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,23 +1,24 @@
 import TextArea from "@/components/customs/TextArea";
+import Btn from "@/components/customs/btn/Btn";
 import TextField from "@/components/customs/textField";
 
 export default function ContactPage() {
   return (
-    <div className=" h-screen md:h-[80vh]  flex flex-col  md:justify-center">
+    <div className=" h-screen md:h-[80vh] flex flex-col md:justify-center">
       <div className="mx-auto w-max">
-      <h1 className="text-3xl">Contact Me</h1>
+        <h1 className="text-3xl">Contact Me</h1>
       </div>
       <div className="flex flex-col gap-5  items-center md:w-1/2 mx-auto mt-10">
         <div className="flex w-full gap-2">
-          <TextField placeholder="Name" className="" />
-          <TextField placeholder="email" className="" />
+          <TextField placeholder="Name" />
+          <TextField placeholder="email" />
         </div>
         <div className="flex w-full gap-2">
-          <TextField placeholder="subject" className="" />
-          <TextField placeholder="Mobile Number" className="" />
+          <TextField placeholder="subject" />
+          <TextField placeholder="Mobile Number" />
         </div>
-        <TextArea className="" rows={5} />
-        <button className="bg-sky-600 text-white p-2 rounded-sm w-full">Submit</button>
+        <TextArea placeholder="text" rows={5} />
+        <Btn className=" w-full">Submit</Btn>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
 import profileImage from "../public/assets/profile.jpg";
+import Btn from "@/components/customs/btn/Btn";
 
 export default function Home() {
   return (
-    <div className=" flex flex-col gap-5 md:gap-0">
+    <div className=" flex flex-col gap-5 md:gap-0 pb-5 md:pb-0">
       <div className="md:h-[80vh] md:flex items-center justify-center md:flex-row-reverse space-y-5 md:space-y-0">
         <section className="md:w-1/3 md:flex-row flex-col flex justify-center items-center">
           <div className="block rounded-full overflow-hidden  border-sky-800 w-[80%] h-[80%] ">
@@ -14,13 +15,13 @@ export default function Home() {
             />
           </div>
         </section>
-        <section className="md:w-2/3 space-y-5  md:text-left text-justify flex flex-col items-center md:items-start">
+        <section className="md:w-2/3 space-y-5  md:text-left text-center flex flex-col items-center md:items-start">
           <h1 className="md:text-5xl text-2xl">Hi, i&apos;m Saleh Shokouhi</h1>
           <h2 className="md:text-xl text-sky-400">Full Stack Web Developer</h2>
           <p>With over four years of experience in web development, I am a self-taught professional skilled in crafting dynamic web applications using popular front-end technologies like Vue and React. My proven track record includes effectively leading small teams of mid-level and junior developers. My knowledge of Git, Git flow, and agile methodologies enables me to work efficiently and collaboratively within development teams. Furthermore, I have a proficient understanding of back-end technologies, including NestJS, MySQL, RabbitMQ, and more.</p>
           <div className="flex flex-col md:flex-row gap-5">
-            <Link href="/contact"><button className="bg-sky-800 text-white px-5 py-2 rounded-md hover:bg-sky-900">Contact Me</button></Link>
-            <button className="border border-sky-800 text-sky-800 px-5 py-2 rounded-md hover:border-sky-900 hover:text-sky-900">Download My Resume ( pdf )</button>
+            <Link href="/contact"><Btn className="w-full">Contact Me</Btn></Link>
+            <Btn outlined>Download My Resume ( pdf )</Btn>
           </div>
         </section>
       </div>
