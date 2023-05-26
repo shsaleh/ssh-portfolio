cbaa6304bf05e865dc505619e3b9f0d8034be206
shokouhi <pgulf.611@gmail.com>
2023-05-27 03:06:05 +0430

    add toast to contact form

--- components/contactForm.tsx
+++ components/contactForm.tsx
@@ -4,9 +4,15 @@ import { FormEvent, useState } from "react";
 import TextField from "./customs/textField";
 import TextArea from "./customs/TextArea";
 import Btn from "./customs/btn";
+import Toast, { ToastPropsType } from "./customs/toast";
 
 export function ContactForm() {
     const [submitloading, setSubmitLoading] = useState(false)
+    const [toast, setToast] = useState<ToastPropsType>({
+        message: '',
+        show: false,
+        type: 'SUCCESS'
+    })
     const sendEmail = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         setSubmitLoading(true)
@@ -14,7 +20,19 @@ export function ContactForm() {
         var object: any = {};
         data.forEach((value, key) => object[key] = value);
         axios.post('/api/sendEmail', object).then(res => {
+            setToast({
+                message: 'The email has been successfully sent.',
+                show: true,
+                type: 'SUCCESS'
+            })
+        }).catch(error => {
+            console.log(error);
 
+            setToast({
+                message: 'Sending the email encountered an error.',
+                show: true,
+                type: 'ERROR'
+            })
         }).finally(() => {
             setSubmitLoading(false)
         })
@@ -33,6 +51,9 @@ export function ContactForm() {
                 <TextArea placeholder="text" rows={5} name="text" />
                 <Btn loading={submitloading} className="w-full">Submit</Btn>
             </form>
+            <Toast message={toast.message} type={toast.type} show={toast.show} onHide={() => {
+                setToast({ ...toast, show: false })
+            }} />
         </div>
     );
 }
