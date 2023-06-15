a93041c7700b62da0250db8317e14cb4d8560476
saleh shokouhi <pgulf.611@gmai.com>
2023-06-15 20:57:13 +0330

    add skill icons

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,24 +1,133 @@
 import SkillCard, { TskillsCart } from "@/components/skillCard";
 
+
+export enum SKILLS_GROUP {
+    LANGUAGES = 'LANGUAGES',
+    LIBRARIES = 'LIBRARIES',
+    FRAMEWORKS = 'FRAMEWORKS',
+    TOOLS = 'TOOLS',
+    DATABASES = 'DATABASES',
+
+}
 export default function SkillsPage() {
     const size = '150px'
-    const skills: Array<TskillsCart & { number: number }> = [
+    const skills: Array<TskillsCart & { number: number, group: SKILLS_GROUP }> = [
         {
             number: 1,
             image: '/assets/icons/nextjs-icon.png',
             name: "next.js",
-            size
+            size,
+            group: SKILLS_GROUP.FRAMEWORKS
         },
         {
-            number: 1,
-            image: "/assets/icons/nestjs.png",
-            name: "nest.js",
-            size
+            number: 2,
+            image: "/assets/icons/css-icon.png",
+            name: "CSS",
+            size,
+            group: SKILLS_GROUP.LANGUAGES
+        },
+        {
+            number: 3,
+            image: "/assets/icons/git-icon.png",
+            name: "git",
+            size,
+            group: SKILLS_GROUP.TOOLS
+        },
+        {
+            number: 4,
+            image: "/assets/icons/gitflow-icon.png",
+            name: "git flow",
+            size,
+            group: SKILLS_GROUP.TOOLS
+        },
+        {
+            number: 5,
+            image: "/assets/icons/html-icon.png",
+            name: "HTML",
+            size,
+            group: SKILLS_GROUP.LANGUAGES
+        },
+        {
+            number: 6,
+            image: "/assets/icons/javascript-icon.png",
+            name: "javaScript",
+            size,
+            group: SKILLS_GROUP.LANGUAGES
+        },
+        {
+            number: 7,
+            image: "/assets/icons/mongodb-icon.png",
+            name: "mongoDB",
+            size,
+            group: SKILLS_GROUP.DATABASES
+        },
+        {
+            number: 8,
+            image: "/assets/icons/mysql-icon.png",
+            name: "mysql",
+            size,
+            group: SKILLS_GROUP.DATABASES
+        },
+        {
+            number: 9,
+            image: "/assets/icons/nodejs-icon.png",
+            name: "node.js",
+            size,
+            group: SKILLS_GROUP.TOOLS
+        },
+        {
+            number: 10,
+            image: "/assets/icons/tailwind-icon.png",
+            name: "tailwind css",
+            size,
+            group: SKILLS_GROUP.TOOLS
+        },
+        {
+            number: 11,
+            image: "/assets/icons/nuxtjs-icon.png",
+            name: "nuxt.js",
+            size,
+            group: SKILLS_GROUP.FRAMEWORKS
+        },
+        {
+            number: 12,
+            image: "/assets/icons/rabbitmq-icon.png",
+            name: "rabbitMQ",
+            size,
+            group: SKILLS_GROUP.TOOLS
+        },
+        {
+            number: 13,
+            image: "/assets/icons/reactjs-icon.png",
+            name: "react.js",
+            size,
+            group: SKILLS_GROUP.LIBRARIES
+        },
+        {
+            number: 14,
+            image: "/assets/icons/vuetifyjs-icon.png",
+            name: "vuetify.js",
+            size,
+            group: SKILLS_GROUP.FRAMEWORKS
+        },
+        {
+            number: 15,
+            image: "/assets/icons/scrum-icon.png",
+            name: "scrum",
+            size,
+            group: SKILLS_GROUP.TOOLS
+        },
+        {
+            number: 16,
+            image: "/assets/icons/vuejs-icon.png",
+            name: "vue.js",
+            size,
+            group: SKILLS_GROUP.FRAMEWORKS
         },
     ]
     return (
-        <section>
-            <div className="flex gap-5">
+        <section className="w-full">
+            <div className="flex gap-5 w-full  flex-wrap">
                 {
                     skills.map((item, index) => {
                         return (
