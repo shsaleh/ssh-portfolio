83abe8078ec57390845618202f636477170a3216
saleh shokouhi <pgulf.611@gmai.com>
2023-06-16 20:38:09 +0330

    optional name

--- components/skillCard.tsx
+++ components/skillCard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-export type TskillsCart = { size: string, image: string, name: string }
+export type TskillsCart = { size: string, image: string, name?: string }
 export default function SkillCard({ size, image, name }: TskillsCart) {
     return (
         <div className={"bg-white p-3 rounded-full flex flex-col text-center justify-center items-center hover:scale-110 transition-all duration-200"} style={{ width: size, height: size }}>
