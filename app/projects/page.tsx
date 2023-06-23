b99bdb41f97e9c2a3d879d4dc35651fb12792f95
shokouhi <pgulf.611@gmail.com>
2023-06-24 02:17:22 +0430

    fix project card and component

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,71 @@
-import ProjectCard from "@/components/projectCard";
+import ProjectCard, { Tproject } from "@/components/projectCard";
+import { skills } from "@/app/skills/page"
+import SkillCard from "@/components/skillCard"
 
 export default function ProjectsPage() {
+    const projects: Array<Tproject & { number: number }> = [
+        {
+            name: 'tamuk.ir',
+            link: 'https://biz.tamuk.ir/',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis beatae asperiores, animi eius voluptatem sunt ullam? Vitae vero provident mollitia autem, tempora, alias natus explicabo, soluta veritatis qui deserunt consequuntur.',
+            skills: [skills['react.js'], skills['vue.js'], skills['nuxt.js']],
+            number: 1
+        },
+        {
+            name: 'tsgco.ir',
+            link: 'https://tsgco.ir/',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis beatae asperiores, animi eius voluptatem sunt ullam? Vitae vero provident mollitia autem, tempora, alias natus explicabo, soluta veritatis qui deserunt consequuntur.',
+            skills: [skills['vue.js'], skills['nuxt.js']],
+            number: 2
+        },
+        {
+            name: 'tsgco.ir',
+            link: 'https://tsgco.ir/',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis beatae asperiores, animi eius voluptatem sunt ullam? Vitae vero provident mollitia autem, tempora, alias natus explicabo, soluta veritatis qui deserunt consequuntur.',
+            skills: [skills['vue.js'], skills['nuxt.js']],
+            number: 2
+        },
+        {
+            name: 'tsgco.ir',
+            link: 'https://tsgco.ir/',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis beatae asperiores, animi eius voluptatem sunt ullam? Vitae vero provident mollitia autem, tempora, alias natus explicabo, soluta veritatis qui deserunt consequuntur.',
+            skills: [skills['vue.js'], skills['nuxt.js']],
+            number: 2
+        },
+        {
+            name: 'tsgco.ir',
+            link: 'https://tsgco.ir/',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis beatae asperiores, animi eius voluptatem sunt ullam? Vitae vero provident mollitia autem, tempora, alias natus explicabo, soluta veritatis qui deserunt consequuntur.',
+            skills: [skills['vue.js'], skills['nuxt.js']],
+            number: 2
+        },
+        {
+            name: 'tsgco.ir',
+            link: 'https://tsgco.ir/',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis beatae asperiores, animi eius voluptatem sunt ullam? Vitae vero provident mollitia autem, tempora, alias natus explicabo, soluta veritatis qui deserunt consequuntur.',
+            skills: [skills['vue.js'], skills['nuxt.js']],
+            number: 2
+        },
+        {
+            name: 'tsgco.ir',
+            link: 'https://tsgco.ir/',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis beatae asperiores, animi eius voluptatem sunt ullam? Vitae vero provident mollitia autem, tempora, alias natus explicabo, soluta veritatis qui deserunt consequuntur.',
+            skills: [skills['vue.js'], skills['nuxt.js']],
+            number: 2
+        },
+    ]
     return (
         <div>
-            <ProjectCard />
+            <div className="h-[80vh] overflow-y-scroll">
+                <div className="flex gap-4 flex-wrap justify-center ">
+                    {
+                        projects.map(project => (
+                            <ProjectCard key={project.number + 'projects'} name={project.name} description={project.description}
+                                link={project.link} skills={project.skills} skillsSize="50px" />
+                        ))
+                    }
+                </div>
+            </div>
         </div>
     )
 
--- components/projectCard.tsx
+++ components/projectCard.tsx
@@ -1,83 +1,34 @@
-import { skills } from "@/app/skills/page"
-import SkillCard from "./skillCard"
+import SkillCard, { Tskills } from "./skillCard"
 
-export default function ProjectCard() {
-    const projects = [
-        {
-            name: 'tamuk.ir',
-            link: 'https://biz.tamuk.ir/',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis beatae asperiores, animi eius voluptatem sunt ullam? Vitae vero provident mollitia autem, tempora, alias natus explicabo, soluta veritatis qui deserunt consequuntur.',
-            tools: [skills['react.js'], skills['vue.js'], skills['nuxt.js']],
-            number: 1
-        },
-        {
-            name: 'tsgco.ir',
-            link: 'https://tsgco.ir/',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis beatae asperiores, animi eius voluptatem sunt ullam? Vitae vero provident mollitia autem, tempora, alias natus explicabo, soluta veritatis qui deserunt consequuntur.',
-            tools: [skills['vue.js'], skills['nuxt.js']],
-            number: 2
-        },
-        {
-            name: 'tsgco.ir',
-            link: 'https://tsgco.ir/',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis beatae asperiores, animi eius voluptatem sunt ullam? Vitae vero provident mollitia autem, tempora, alias natus explicabo, soluta veritatis qui deserunt consequuntur.',
-            tools: [skills['vue.js'], skills['nuxt.js']],
-            number: 2
-        },
-        {
-            name: 'tsgco.ir',
-            link: 'https://tsgco.ir/',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis beatae asperiores, animi eius voluptatem sunt ullam? Vitae vero provident mollitia autem, tempora, alias natus explicabo, soluta veritatis qui deserunt consequuntur.',
-            tools: [skills['vue.js'], skills['nuxt.js']],
-            number: 2
-        },
-        {
-            name: 'tsgco.ir',
-            link: 'https://tsgco.ir/',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis beatae asperiores, animi eius voluptatem sunt ullam? Vitae vero provident mollitia autem, tempora, alias natus explicabo, soluta veritatis qui deserunt consequuntur.',
-            tools: [skills['vue.js'], skills['nuxt.js']],
-            number: 2
-        },
-        {
-            name: 'tsgco.ir',
-            link: 'https://tsgco.ir/',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis beatae asperiores, animi eius voluptatem sunt ullam? Vitae vero provident mollitia autem, tempora, alias natus explicabo, soluta veritatis qui deserunt consequuntur.',
-            tools: [skills['vue.js'], skills['nuxt.js']],
-            number: 2
-        },
-        {
-            name: 'tsgco.ir',
-            link: 'https://tsgco.ir/',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis beatae asperiores, animi eius voluptatem sunt ullam? Vitae vero provident mollitia autem, tempora, alias natus explicabo, soluta veritatis qui deserunt consequuntur.',
-            tools: [skills['vue.js'], skills['nuxt.js']],
-            number: 2
-        },
-    ]
+export type Tproject = {
+    name: string
+    link: string
+    description: string
+    skills: Array<Tskills>
+}
+
+export type TprojectCard = Tproject & {
+    skillsSize: string
+}
+export default function ProjectCard({ name, link, description, skills, skillsSize }: TprojectCard) {
+
 
     return (
-        <div className="h-[80vh] overflow-y-scroll">
-            <div className="flex gap-4 flex-wrap justify-center lg:justify-normal">
-                {projects.map(project => {
+        <div className="border border-solid border-sky-500  rounded-2xl max-w-sm">
+            <div className="p-5">
+                <h2 className="underline mb-2"><a className="hover:text-sky-500" href={link} target="_blank" rel='nofollow'>{name}</a></h2>
+                <p>{description}</p>
+            </div>
+            <div className=" border-t-2 border-solid border-sky-500"></div>
+            <div className="flex justify-center gap-2 p-3">{
+                skills.map(skill => {
                     return (
-                        <div key={project.number} className="border border-solid border-sky-500  rounded-2xl max-w-sm">
-                            <div className="p-5">
-                                <h2 className="underline mb-2"><a className="hover:text-sky-500" href={project.link} target="_blank" rel='nofollow'>{project.name}</a></h2>
-                                <p>{project.description}</p>
-                            </div>
-                            <div className=" border-t-2 border-solid border-sky-500"></div>
-                            <div className="flex justify-center gap-2 p-3">{
-                                project.tools.map(tool => {
-                                    return (
-                                        <div key={tool.number + 'skills' + project.number}>
-                                            <SkillCard image={tool.image} size="80px" />
-                                        </div>
-                                    )
-                                })
-                            }</div>
+                        <div key={skill.name + 'skills' + name}>
+                            <SkillCard image={skill.image} size={skillsSize} />
                         </div>
                     )
-                })}
-            </div>
+                })
+            }</div>
         </div>
     )
 
