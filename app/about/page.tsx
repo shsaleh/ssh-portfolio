b872a8cbb4f493084b8f38d05431906568125296
shokouhi <pgulf.611@gmail.com>
2023-06-25 02:48:26 +0330

    fix about page style

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,10 +11,10 @@ export default function about() {
                 <h2 className="md:text-xl text-sky-400">Full Stack Web Developer</h2>
                 <p>I started this career 4 years ago when I faced some difficulties in my previous career as an electrical engineer. I love being an engineer and I have a passion for all engineering majors. However, in my country, being an electrical engineer is very challenging due to a poor workspace culture, an inadequate education system, and low salary. Therefore, I made the decision to switch my career to software engineering. This way, I could learn coding on my own and work with the best colleagues I have ever had.</p>
                 <p>I have worked for several companies and have experience in building various types of web applications. These include online shops, marketplaces, crypto trading platforms, and more.</p>
-                <div className="flex flex-col md:flex-row gap-3 md:mt-10">
-                    <Link href="/skills"><Btn className="w-[300px]">My Skills</Btn></Link>
-                    <Link href="/projects"><Btn className="w-[300px]">My Projects</Btn></Link>
-                    <Link href="/contact"><Btn className="w-[300px]">Contact Me</Btn></Link>
+                <div className="flex flex-col w-full items-center lg:flex-row gap-3 lg:mt-10">
+                    <Link className="w-full" href="/skills"><Btn className="w-full">My Skills</Btn></Link>
+                    <Link className="w-full" href="/projects"><Btn className="w-full">My Projects</Btn></Link>
+                    <Link className="w-full" href="/contact"><Btn className="w-full">Contact Me</Btn></Link>
                 </div>
             </section>
             <section className="md:w-1/3 md:flex-row flex-col flex justify-center items-center">
