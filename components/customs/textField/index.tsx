407fd5f70bd4ffab92a07de67721201ab716606a
sh.saleh <pgulf.611@gmail.com>
2023-07-07 22:53:17 +0330

    text filed integrated with react hook form

--- components/customs/textField/index.tsx
+++ components/customs/textField/index.tsx
@@ -1,11 +1,25 @@
 'use client';
-import React, { InputHTMLAttributes } from "react";
+import { useForm } from 'react-hook-form';
+import React, { InputHTMLAttributes, useContext } from "react";
+import { formContext } from '../form';
 
 type TextFieldPropsType = InputHTMLAttributes<{}>
-export default function TextField(props: TextFieldPropsType) {
-    return (
-        <>
-            <input {...props} className={" p-2 bg-white rounded-sm bg-opacity-0 focus:bg-opacity-10 outline-2 outline-sky-600 outline transition-all ease-in-out duration-150 w-full " + props.className} />
-        </>
-    );
+export default function TextField(props: TextFieldPropsType & { Required?: boolean, rules?: { [key: string]: (value: string) => string | boolean } }) {
+    const context = useContext(formContext)
+    if (context) {
+        const { register, formState } = context;
+        return (
+            <>
+                <div className={props.className}>
+                    <input {...(props.name && register(props.name, {
+                        required: props.Required,
+                        ...(props.rules && { validate: props.rules }),
+                    }))} {...props} className={"w-full p-2 bg-white rounded-sm bg-opacity-0 focus:bg-opacity-10 outline-2 outline-sky-600 outline transition-all ease-in-out duration-150"} />
+                    <span className='whitespace-pre'>{props.name && formState.errors[props.name]?.message as string} </span>
+                </div>
+            </>
+        );
+    } else {
+        throw new Error('context is null')
+    }
 }
